3ff0ddf8bd428e31bcb36e6db0e90b01a90e6e95
YasirmalIk <110712933+malikcancode@users.noreply.github.com>
2025-04-12 13:17:39 +0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,35 @@
 const projectData = [
   {
+    id: 10,
+    title: "Priceoye",
+    subheading: "Ecommerce store clone",
+    link: "https://price-oye-clone-xi.vercel.app/",
+  },
+  {
+    id: 8,
+    title: "EnhanceX",
+    subheading: "Upscale your Image",
+    link: "https://enhance-x-image-upscaler.vercel.app/",
+  },
+  {
+    id: 9,
+    title: "PawsAndClaws",
+    subheading: "small-town clinic",
+    link: "https://paws-and-claws-vet.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "AppleVision",
+    subheading: "Welcome to spatial computing",
+    link: "https://apple-vision-henna.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Golf Website",
+    subheading: "Eat Drink Play",
+    link: "https://sidcup-family-golf-topaz.vercel.app/",
+  },
+  {
     id: 1,
     title: "FlavorFusion",
     subheading: "Flavors that Tell a Story",
@@ -24,78 +54,9 @@ const projectData = [
     link: "https://magma-animated-website.netlify.app/",
   },
   {
-    id: 5,
-    title: "AppleVision",
-    subheading: "Welcome to spatial computing",
-    link: "https://apple-vision-henna.vercel.app/",
-  },
-  {
-    id: 6,
-    title: "Family Website",
-    subheading: "Eat Drink Play",
-    link: "https://sidcup-family-golf-topaz.vercel.app/",
-  },
-  {
     id: 7,
     title: "Movie Search",
     subheading: "Search Your Movie",
     link: "https://movie-search-app-five-delta.vercel.app/",
   },
-    {
-    id: 8,
-    title: "EnhanceX",
-    subheading: "Upscale your Image",
-    link: "https://enhance-x-image-upscaler.vercel.app/",
-  },
-     {
-    id: 9,
-    title: "PawsAndClaws",
-    subheading: "small-town clinic",
-    link: "https://paws-and-claws-vet.vercel.app/",
-  },
-    {
-    id: 10,
-    title: "Priceoye",
-    subheading: "Ecommerce store clone",
-    link: "https://price-oye-clone-xi.vercel.app/",
-  },
 ];
-
-function Projects() {
-  return (
-    <>
-      <div className="flex flex-col items-end p-8">
-        <h2 className="sm:text-5xl leading-[3rem] sm:leading-none md:text-6xl text-4xl text-white mb-16">
-          Recent Projects and{" "}
-          <span className="text-[#8B48E8]">Achievements</span>
-        </h2>
-
-        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {projectData.map((project) => (
-            <a
-              key={project.id}
-              href={project.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block"
-            >
-              <div className="bg-[#272829] hover:bg-[#006d77] rounded-lg overflow-hidden shadow-lg p-6 transition-all duration-300 hover:shadow-xl">
-                <div className="flex items-end justify-end">
-                  <i className="ri-arrow-right-up-line rounded-full border bg-transparent p-2 hover:bg-[#8B48E8] text-white text-xl transition-transform duration-300 hover:scale-125"></i>
-                </div>
-                <h3 className="text-3xl tracking-wider font-semibold text-white mb-2">
-                  {project.title}
-                </h3>
-                <h4 className="text-[16px] px-2 font-light tracking-wider mt-8 bg-[#8B48E8] text-white mb-4">
-                  {project.subheading}
-                </h4>
-              </div>
-            </a>
-          ))}
-        </div>
-      </div>
-    </>
-  );
-}
-
-export default Projects;
